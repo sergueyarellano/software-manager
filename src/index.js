d713ecdecec2829b4acf3f6b774c76abea305935
Serguey Arellano Martínez <sarellano85@gmail.com>
2018-01-23 19:34:44 +0100

    add hot reload

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('My Minimal React Webpack Babel Setup');
+import React from 'react'
+import ReactDOM, { render } from 'react-dom'
+
+const title = 'woow I have hot reloading'
+
+render(
+  <div>{title}</div>,
+  document.getElementById('root')
+)
+
+module.hot.accept()
